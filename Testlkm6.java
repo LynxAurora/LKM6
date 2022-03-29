6b05a78d79ef1586d053f3714bdff21b7a537ea1
IstiNA <102270816+LynxAurora@users.noreply.github.com>
2022-03-29 14:02:13 +0700

    Create Testlkm6.java

---
+++ Testlkm6.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lkm6;
+
+/**
+ *
+ * @author ACER
+ */
+public class Testlkm6 {
+
+    /**
+     * @param args the command line arguments
+     */
+//Isti Nur Azizah
+//NIM : 2008096041
+//TI 4B
+    public static void main(String[] args ) {
+        Enkapsulasi x = new Enkapsulasi (0);
+        x.input();
+        x.cekangka();
+        System.out.println (x.getAngka() + " adalah " + x.cekangka());
+    }
+        
+}
